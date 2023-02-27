75e73c161af7395864ba0a77bb890f51965654cb
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-02-27 23:30:48 +0300

    исправил отправку видео в отчет

--- attach.py
+++ attach.py
@@ -3,7 +3,7 @@ from allure_commons.types import AttachmentType
 
 
 def add_video(browser):
-    video_url = "https://app-automate.browserstack.com/s3-upload/bs-video-logs-euw/s3.eu-west-1/" + browser.driver.session_id + '/video-' + 'browser.driver.session_id' + ".mp4"
+    video_url = "https://app-automate.browserstack.com/s3-upload/bs-video-logs-euw/s3.eu-west-1/" + browser.driver.session_id + '/video-' + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
